cfd49a9b2eb019dc18a2ce2c8fd53b62a59c4ee5
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-07-05 01:16:54 +0200

    Modify update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,6 +29,7 @@
             <div class="card-header">
                 AGGIUNGI UN NUOVO POST
             </div>
+
             <div class="card-body">
                 <h5 class="card-title"> Crea da zero un nuovo post</h5>
                 <a href="{{route('post.create')}}" class="btn btn-primary"> ADD </a>
@@ -44,8 +45,22 @@
             @foreach ($posts as $post)
             <div class="mb-5">
                 <h1>{{ $post['title'] }}</h1>
+
                 <p>{!! nl2br(e($post->body)) !!}</p>
-                <a href="{{ route('post.show', $post['id']) }}"> DETTAGLI </a>
+                <a href="{{ route('post.show', $post['id']) }}" class="btn btn-secondary"> Dettagli </a>
+                @auth
+
+                <a href="{{ route('post.edit', $post['id']) }}" class="btn btn-primary">Modifica</a>
+                <form class="d-inline-block" action="{{ route('post.destroy', ['id' => $post->id]) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger delete">
+                        Elimina
+                    </button>
+                </form>
+
+                <hr>
+                @endauth
             </div>
             @endforeach
         </div>
