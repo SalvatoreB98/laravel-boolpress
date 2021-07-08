aae784e9ec4bff42f20ecd05d0a0bb78a4191d28
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-07-08 15:29:16 +0200

    Validation OK

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,38 +1,50 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Tag;
 use App\Category;
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Facades\Storage;
+
 class PostController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         $tags = Tag::all();
         $categories = Category::all();
         $data = [
             "categories" => $categories,
             "tags" => $tags
         ];
-        return view("post.create",$data);
+        return view("post.create", $data);
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'category_id' => "nullable|exists:categories,id"
+        ]);
         $formData = $request->all();
         $newPost = new Post();
         $newPost->fill($formData);
         $newPost->save();
-        if(key_exists("tags",$formData)){
+        if (key_exists("tags", $formData)) {
             $newPost->tags()->attach($formData["tags"]);
         }
         return redirect('/');
     }
-    public function show($id){
+    public function show($id)
+    {
         $post = Post::findOrFail($id);
         $user = $post->user;
         $category = $post->category;
-        return view('post.show', ["post"=> $post, "user"=> $user, "category"=>$category]);
+        return view('post.show', ["post" => $post, "user" => $user, "category" => $category]);
     }
-    public function edit($id){
+    public function edit($id)
+    {
         $categories = Category::all();
         $tags = Tag::all();
         $post = Post::findOrFail($id);
@@ -43,17 +55,20 @@ class PostController extends Controller
         ];
         return view('post.edit', $data);
     }
-    public function update(Request $request,$id){
+    public function update(Request $request, $id)
+    {
         $formData = $request->all();
         $post = Post::findOrFail($id);
         $post->tags()->detach();
-        if(key_exists("tags",$formData)){
+        // Storage::put("files",$formData["file"]);
+        if (key_exists("tags", $formData)) {
             $post->tags()->attach($formData["tags"]);
         }
         $post->update($formData);
-        return redirect()->to('post/'. $id);
+        return redirect()->to('post/' . $id);
     }
-    public function destroy($id){
+    public function destroy($id)
+    {
         $post = Post::findOrFail($id);
         $post->tags()->detach();
         $post->delete();
